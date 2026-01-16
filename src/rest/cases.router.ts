36ef026b4b129211577382c2693f74e47a99e4e8
dennis <dp.jandow@gmail.com>
2026-01-16 23:33:50 +0100

    add body format to swagger

--- src/rest/cases.router.ts
+++ src/rest/cases.router.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import { generateCase } from "../services/cases.service.js";
-import { CaseGenerationRequestSchema } from "../dtos/CaseGenerationRequest.js";
-import { CaseGenerationResponseSchema } from "../dtos/CaseGenerationResponse.js";
+import {
+  CaseGenerationRequestSchema,
+  type CaseGenerationRequest,
+} from "../dtos/CaseGenerationRequest.js";
+import {
+  CaseGenerationResponseSchema,
+  type CaseGenerationResponse,
+} from "../dtos/CaseGenerationResponse.js";
 import { IcdToDiseaseName } from "@/services/diseases.service.js";
 import { AppError } from "@/errors/AppError.js";
 
@@ -12,62 +18,97 @@ router.use((_req, _res, next) => {
   next();
 });
 
-router.post("/", async (req, res) => {
-  // take Case Generation Request from body
-  const bodyResult = CaseGenerationRequestSchema.safeParse(req.body);
+router.post(
+  "/",
+  async (
+    req: express.Request<CaseGenerationRequest>,
+    res: express.Response<CaseGenerationResponse>
+  ) => {
+    /*  #swagger.requestBody = {
+            required: true,
+            content: {
+                "application/json": {
+                    schema: {
+                        $ref: "#/components/schemas/CaseGenerationRequest"
+                    }  
+                }
+            }
+        } 
+    */
+    const bodyResult = CaseGenerationRequestSchema.safeParse(req.body);
 
-  if (!bodyResult.success) {
-    res.status(400).json({
-      message: "Invalid request body",
-      errors: bodyResult.error.issues,
-    });
-    return;
-  }
-
-  let { diagnosis } = bodyResult.data;
-  const { icd, context, generationFlags, language } = bodyResult.data;
-
-  // fill diagnosis and icdCode - zod makes sure that at least one is filled
-  if (!diagnosis) {
-    // if diagnosis is missing, icd is provided
-    diagnosis = await IcdToDiseaseName(icd!);
-    // verify that is set now, otherwise return error
-    if (!diagnosis) {
-      res.status(400).json({ message: "No diagnosis found for icd" });
-      return;
-    }
-  }
-
-  try {
-    const caseData = await generateCase(
-      icd,
-      diagnosis,
-      generationFlags,
-      context,
-      language
-    );
-    const response = CaseGenerationResponseSchema.parse(caseData);
-    res.status(200).json(response);
-  } catch (error) {
-    console.error(error);
-    if (error instanceof AppError) {
-      res.status(error.statusCode).json({
+    if (!bodyResult.success) {
+      res.status(400).json({
         error: {
-          code: error.code,
-          message: error.message,
-          details: error.details,
+          code: "INVALID_REQUEST_BODY",
+          message: "Invalid request body",
+          details: JSON.stringify(bodyResult.error.issues),
         },
       });
       return;
     }
 
-    res.status(500).json({
-      error: {
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Internal server error",
-      },
-    });
+    let { diagnosis } = bodyResult.data;
+    const { icd, context, generationFlags, language } = bodyResult.data;
+
+    // fill diagnosis and icdCode - zod makes sure that at least one is filled
+    if (!diagnosis) {
+      // if diagnosis is missing, icd is provided
+      diagnosis = await IcdToDiseaseName(icd!);
+      // verify that is set now, otherwise return error
+      if (!diagnosis) {
+        res.status(400).json({
+          error: {
+            code: "INVALID_REQUEST_BODY",
+            message: "No diagnosis found for icd",
+          },
+        });
+        return;
+      }
+    }
+
+    try {
+      const caseData = await generateCase(
+        icd,
+        diagnosis,
+        generationFlags,
+        context,
+        language
+      );
+      const response = CaseGenerationResponseSchema.parse(caseData);
+
+      /* #swagger.responses[200] = {
+            content: {
+                "application/json": {
+                    schema:{
+                        $ref: "#/components/schemas/CaseGenerationResponse"
+                    }
+                }           
+            }
+        }   
+    */
+      res.status(200).json(response);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof AppError) {
+        res.status(error.statusCode).json({
+          error: {
+            code: error.code,
+            message: error.message,
+            details: error.details,
+          },
+        });
+        return;
+      }
+
+      res.status(500).json({
+        error: {
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Internal server error",
+        },
+      });
+    }
   }
-});
+);
 
 export default router;

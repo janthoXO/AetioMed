aaa4cae9fa58be64fd91744d7841f555e0a4c824
dennis <dp.jandow@gmail.com>
2026-03-18 16:07:30 +0800

    switch to a more parallel graph approach with less cot nodes

---
+++ src/02graphs/case-parallel/02generation/index.ts
@@ -0,0 +1,175 @@
+import { END, Send, START, StateGraph } from "@langchain/langgraph";
+import { GlobalStateSchema } from "../state.js";
+import z from "zod";
+import { generateChiefComplaintFromOutline } from "@/03aigateway/chiefComplaint.aigateway.js";
+import { generateCaseCoT as generateCaseCoTGateway } from "@/03aigateway/case.aigateway.js";
+import { emitTrace } from "@/utils/tracing.js";
+import { generatePatientFromOutline } from "@/03aigateway/patient.aigateway.js";
+import { generateAnamnesisFromOutline } from "@/03aigateway/anamnesis.aigateway.js";
+import { generateProceduresFromOutline } from "@/03aigateway/procedures.aigateway.js";
+
+const GenerationGraphStateSchema = GlobalStateSchema.extend({
+  cot: z.string(),
+});
+
+type GenerationGraphState = z.infer<typeof GenerationGraphStateSchema>;
+
+async function generateCaseCoT(
+  state: GenerationGraphState
+): Promise<Pick<GenerationGraphState, "cot">> {
+  state.cot = await generateCaseCoTGateway(
+    state.diagnosis,
+    state.generationFlags,
+    state.userInstructions
+  ).catch((error) => {
+    emitTrace(`[GenerationGraph] Error generating case CoT: ${error}`, {
+      category: "error",
+    });
+    throw error;
+  });
+
+  emitTrace(`[GenerationGraph] Successfully generated case CoT:\n${state.cot}`);
+
+  return { cot: state.cot };
+}
+
+async function generateCaseOutline(
+  state: GenerationGraphState
+): Promise<Pick<GenerationGraphState, "outline">> {
+  state.outline = await generateCaseCoTGateway(
+    state.diagnosis,
+    state.generationFlags,
+    state.userInstructions
+  ).catch((error) => {
+    emitTrace(`[GenerationGraph] Error generating case outline: ${error}`, {
+      category: "error",
+    });
+    throw error;
+  });
+
+  emitTrace(
+    `[GenerationGraph] Successfully generated case outline:\n${state.outline}`
+  );
+
+  return { outline: state.outline };
+}
+
+async function generatePatient(state: GenerationGraphState) {
+  emitTrace(`[GenerationGraph] Starting generation of patient fields...`);
+  state.case.patient = await generatePatientFromOutline(
+    state.diagnosis,
+    state.outline,
+    state.userInstructions
+  ).catch((error) => {
+    emitTrace(`[GenerationGraph] Error generating patient: ${error}`, {
+      category: "error",
+    });
+    throw error;
+  });
+
+  emitTrace(
+    `[GenerationGraph] Successfully generated patient: ${JSON.stringify(
+      state.case.patient,
+      null,
+      2
+    )}`
+  );
+
+  return { case: state.case };
+}
+
+async function generateChiefComplaint(
+  state: GenerationGraphState
+): Promise<Pick<GenerationGraphState, "case">> {
+  emitTrace(`[GenerationGraph] Starting generation of chief complaint...`);
+  state.case.chiefComplaint = await generateChiefComplaintFromOutline(
+    state.diagnosis,
+    state.outline,
+    state.userInstructions
+  ).catch((error) => {
+    emitTrace(`[GenerationGraph] Error generating chief complaint: ${error}`, {
+      category: "error",
+    });
+    throw error;
+  });
+
+  emitTrace(
+    `[GenerationGraph] Successfully generated chief complaint: ${state.case.chiefComplaint}`
+  );
+
+  return { case: state.case };
+}
+
+async function generateAnamnesis(state: GenerationGraphState) {
+  emitTrace(`[GenerationGraph] Starting generation of anamnesis fields...`);
+  state.case.anamnesis = await generateAnamnesisFromOutline(
+    state.diagnosis,
+    state.outline,
+    state.userInstructions,
+    state.anamnesisCategories
+  ).catch((error) => {
+    emitTrace(`[GenerationGraph] Error generating anamnesis: ${error}`, {
+      category: "error",
+    });
+    throw error;
+  });
+
+  emitTrace(
+    `[GenerationGraph] Successfully generated anamnesis:\n${state.case.anamnesis}`
+  );
+
+  return { case: state.case };
+}
+
+async function generateProcedures(state: GenerationGraphState) {
+  emitTrace(`[GenerationGraph] Starting generation of procedures...`);
+  state.case.procedures = await generateProceduresFromOutline(
+    state.diagnosis,
+    state.outline,
+    state.userInstructions
+  ).catch((error) => {
+    emitTrace(`[GenerationGraph] Error generating procedures: ${error}`, {
+      category: "error",
+    });
+    throw error;
+  });
+
+  emitTrace(
+    `[GenerationGraph] Successfully generated procedures:\n${state.case.procedures}`
+  );
+
+  return { case: state.case };
+}
+
+export const generationGraph = new StateGraph(GenerationGraphStateSchema)
+  .addNode("case_cot_generate", generateCaseCoT)
+  .addNode("case_outline_generate", generateCaseOutline)
+  .addNode("patient_generate", generatePatient)
+  .addNode("chief_complaint_generate", generateChiefComplaint)
+  .addNode("anamnesis_generate", generateAnamnesis)
+  .addNode("procedures_generate", generateProcedures)
+
+  .addEdge(START, "case_cot_generate")
+  .addEdge("case_cot_generate", "case_outline_generate")
+  .addConditionalEdges(
+    "case_outline_generate",
+    (state): Send[] => {
+      return [
+        new Send("patient_generate", state),
+        new Send("chief_complaint_generate", state),
+        new Send("anamnesis_generate", state),
+        new Send("procedures_generate", state),
+      ];
+    },
+    [
+      "patient_generate",
+      "chief_complaint_generate",
+      "anamnesis_generate",
+      "procedures_generate",
+    ]
+  )
+  .addEdge("patient_generate", END)
+  .addEdge("chief_complaint_generate", END)
+  .addEdge("anamnesis_generate", END)
+  .addEdge("procedures_generate", END)
+  .compile();
